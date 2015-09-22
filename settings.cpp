1b9e9fc015fbc1f7f7ee8e096a9286dc5d2bc7ee
Marc Clint Dion <marcclintdion@Marcs-iMac.local>
2015-09-22 00:50:07 -0700

    init

---
+++ settings.cpp
@@ -0,0 +1,10 @@
+var         facing_UP_LIGHT_POSITION_01[]            =  {1.90998, 10.6199, 22.4007, 1.0};
+
+var         facing_UP_ATTENUATION            =  -0.0139;
+var         facing_UP_SHININESS              =  0.0488003;
+
+
+//------------------------------------------------------------------------------ 
+        var         moveSet[]                  =  {-0.4, -0.1, -5.59998, 1.0};
+        var         rotateModelWithLeftMouse[] =  {4.20004, -3.89999, 0.0, 0.0};
+
